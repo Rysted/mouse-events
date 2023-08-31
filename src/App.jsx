bd8d2be12806515b541478194a8f5e2cfdee2fca
SeezDev <sebastianszz@hotmail.com>
2023-08-31 17:58:01 -0300

    Agregando Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import data from "./data";
 import Icon from "./components/Icon";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
@@ -33,47 +34,50 @@ const App = () => {
   };
 
   return (
-    <main
-      onWheel={handleWheel}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onContextMenu={handleContextMenu}
-    >
-      <div className="mouse">
-        {data.map((svg, index) => {
-          const {
-            name,
-            width,
-            height,
-            viewBox,
-            path,
-            stroke,
-            strokeWidth,
-            strokeLinejoin,
-          } = svg;
+    <>
+      <main
+        onWheel={handleWheel}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onContextMenu={handleContextMenu}
+      >
+        <div className="mouse">
+          {data.map((svg, index) => {
+            const {
+              name,
+              width,
+              height,
+              viewBox,
+              path,
+              stroke,
+              strokeWidth,
+              strokeLinejoin,
+            } = svg;
 
-          return (
-            <Icon
-              key={index}
-              className={name}
-              width={width}
-              height={height}
-              viewBox={viewBox}
-              path={path}
-              pressed={name === pressed}
-              /* Únicamente para el evento de desplazamiento hacia arriba y hacia abajo */
-              stroke={stroke}
-              strokeWidth={strokeWidth}
-              strokeLinejoin={strokeLinejoin}
-            />
-          );
-        })}
+            return (
+              <Icon
+                key={index}
+                className={name}
+                width={width}
+                height={height}
+                viewBox={viewBox}
+                path={path}
+                pressed={name === pressed}
+                /* Únicamente para el evento de desplazamiento hacia arriba y hacia abajo */
+                stroke={stroke}
+                strokeWidth={strokeWidth}
+                strokeLinejoin={strokeLinejoin}
+              />
+            );
+          })}
 
-        <img className="design" src="/design.png" alt="mouse design" />
-        <img className="razer" src="/razer.png" alt="mouse razer" />
-        <img className="shell" src="/shell.png" alt="mouse shell" />
-      </div>
-    </main>
+          <img className="design" src="/design.png" alt="mouse design" />
+          <img className="razer" src="/razer.png" alt="mouse razer" />
+          <img className="shell" src="/shell.png" alt="mouse shell" />
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/SeezDev">GitHub</a>
+      <span className="eventsNone">|</span>
+      <a href="https://github.com/SeezDev/mouse-events-react">Project</a>
+      <p className="eventsNone">© 2023 SeezDev</p>
+    </footer>
+  );
+};
+
+export default Footer;
