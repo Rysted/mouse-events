68cda7d7052d8415bf5deb774963f597dc964755
SeezDev <sebastianszz@hotmail.com>
2023-08-31 01:02:40 -0300

    Separando los datos de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,11 @@
+import { useState } from "react";
+import data from "./data/data";
+import Button from "./components/Button";
+
 import "./App.css";
 import shell from "./assets/shell.png";
 import design from "./assets/design.png";
 import razer from "./assets/razer.png";
-import Button from "./components/Button";
-import { useState } from "react";
-
-const data = [
-  {
-    id: 0,
-    name: "left",
-    width: "277",
-    height: "285",
-    viewBox: "0 0 277 285",
-    path: "M0 252L34.5 201L69.5 150L108.5 96L149 43L162.5 27.5L176.298 12.9501L218.5 0.5L257.327 57.5938L276.79 98.5L208.29 183.75L139.79 269L108.79 276.5L78.79 285L0 263.219V252Z",
-  },
-  {
-    id: 1,
-    name: "middle",
-    width: "99",
-    height: "105",
-    viewBox: "0 0 99 105",
-    path: "M0 89L2.5 76L6 64L13.5 48.5L18 41L23.5 34.5L33.5 23.5L45 14.5L58.7601 6.10361L66.5 2.5L75 0L87 7L98.5 14.5L95 22L78.5 41.5L63 61.5L30.5 101.637L27 103.5L23 104.5L19.5 103.5L16.67 101.637L11.0859 94.9917L4.5 92L0 89Z",
-  },
-  {
-    id: 2,
-    name: "right",
-    width: "245",
-    height: "274",
-    viewBox: "0 0 245 274",
-    path: "M35.5 273.5V269.5V266.5L36.4287 264.5L67 226.5L97.4287 188L137.5 141.5L158.5 118L179.5 95L221 54L238.5 40L241.5 25L244.5 10L238.5 8L223 7.5L202.262 4.70496L181.5 2L162 0.5L156.5 1L151.5 2.5L147 3.5L142.5 6L119.5 24.5L98 43.5L76 65L55 87.5L37 110L19.5 132.5L0 161L1 224L35.5 273.5Z",
-  },
-  {
-    id: 3,
-    name: "down",
-    width: "93",
-    height: "105",
-    viewBox: "0 0 93 105",
-    path: "M2.82323 93.0905L42.5173 46.9737L27.1989 33.7888L88.7742 3.55973L68.0479 68.9487L52.7295 55.7636L13.0354 101.88L2.82323 93.0905Z",
-    stroke: "#416B92",
-    strokeWidth: "7",
-    strokeLinejoin: "bevel",
-  },
-  {
-    id: 4,
-    name: "up",
-    width: "87",
-    height: "110",
-    viewBox: "0 0 87 110",
-    path: "M83.6604 11.3125L46.9746 59.8568L63.0994 72.0426L3.56782 106.12L20.1003 39.5474L36.2249 51.7331L72.9107 3.18879L83.6604 11.3125Z",
-    stroke: "#416B92",
-    strokeWidth: "7",
-    strokeLinejoin: "bevel",
-  },
-];
 
 const App = () => {
   const handleContextMenu = (e) => {
